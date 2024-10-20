0f39ef466d4960a385b3e4f17b60d5d5a1b832da
Enayut <vishvignair@gmail.com>
2024-10-20 12:05:01 +0530

    Created Landing Page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,68 +1,51 @@
+import landing from '../assets/landing.jpeg';
 export default function Landing() {
     return (
-        <div class="relative">
-            
-            <section class="bg-yellow-50 overflow-hidden">
-                <div class="flex flex-col lg:flex-row lg:items-stretch lg:min-h-[800px]">
-                    <div class="relative flex items-center justify-center w-full lg:order-2 lg:w-7/12">
-                        <div class="absolute bottom-0 right-0 hidden lg:block">
-                            <img class="object-contain w-auto h-48" src="https://cdn.rareblocks.xyz/collection/celebration/images/hero/3/curved-lines.png" alt="" />
+        <div className="relative">
+
+            <section className="bg-background overflow-hidden">
+                <div className="flex flex-col lg:flex-row lg:items-stretch lg:min-h-[800px]">
+                    <div className="relative flex items-center justify-center w-full lg:order-2 lg:w-7/12">
+                        <div className="absolute bottom-0 right-0 hidden lg:block">
+                            <img className="object-contain w-auto h-48" src="https://cdn.rareblocks.xyz/collection/celebration/images/hero/3/curved-lines.png" alt="" />
                         </div>
 
-                        <div class="relative px-4 pt-24 pb-16 text-center sm:px-6 md:px-24 2xl:px-32 lg:py-24 lg:text-left">
-                            <h1 class="text-4xl font-bold text-black sm:text-6xl xl:text-8xl">
-                                Get it done.<br />
-                                Fast, Easy.
+                        <div className="relative px-4 pt-24 pb-16 text-center sm:px-6 md:px-24 2xl:px-32 lg:py-24 lg:text-left">
+                            <h1 className="text-4xl font-bold text-black sm:text-6xl xl:text-8xl">
+                                Empower.<br />
+                                Elevate, Thrive.
                             </h1>
-                            <p class="mt-8 text-xl text-black">We help you to make your remote work life easier. Build a distruction free working experience.</p>
+                            <p className="mt-8 mb-6 text-xl text-black">We help you to make your remote work life easier. Build a distruction free working experience.</p>
 
-                            <form action="#" method="POST" class="max-w-xl mx-auto mt-8 bg-white lg:mx-0 sm:bg-transparent lg:mt-12 rounded-xl">
-                                <div class="p-4 sm:p-2 sm:bg-white sm:border-2 sm:border-transparent sm:rounded-full sm:focus-within:border-orange-500 sm:focus-within:ring-1 sm:focus-within:ring-orange-500">
-                                    <div class="flex flex-col items-start sm:flex-row">
-                                        <div class="flex-1 w-full min-w-0">
-                                            <div class="relative text-gray-400 focus-within:text-gray-600">
-                                                <label for="email" class="sr-only"></label>
-                                                <input
-                                                    type="email"
-                                                    name="email"
-                                                    id="email"
-                                                    placeholder="Enter email to get started"
-                                                    class="block w-full px-4 py-4 text-base text-center text-black placeholder-gray-500 transition-all duration-200 border-transparent rounded-full sm:text-left focus:border-transparent focus:ring-0 caret-orange-500"
-                                                    required=""
-                                                />
-                                            </div>
-                                        </div>
-
-                                        <button type="submit" class="inline-flex items-center justify-center w-full px-4 py-4 mt-4 font-semibold text-white transition-all duration-200 bg-orange-500 border border-transparent rounded-full sm:w-auto sm:ml-4 sm:mt-0 hover:bg-orange-600 focus:bg-orange-600">
-                                            Try 14 days free
-                                        </button>
-                                    </div>
-                                </div>
-                            </form>
-                            <p class="mt-5 text-base text-black">Instant access . No credit card required</p>
+                            <button className="inline-flex bg-secondary items-center justify-center w-full px-4 py-4 mt-4 font-semibold text-white transition-all duration-200 bg-orange-500 border border-transparent rounded-full sm:w-auto sm:ml-4 sm:mt-0 hover:bg-orange-600 focus:bg-orange-600">
+                                Get Started
+                            </button>
+                            <p className="mt-5 text-base text-black">Instant access . No credit card required</p>
                         </div>
 
-                        <div class="absolute right-0 z-10 -bottom-16 lg:top-24 lg:-left-20">
-                            <img class="w-32 h-32 md:w-40 md:h-40" src="https://cdn.rareblocks.xyz/collection/celebration/images/hero/3/circular-text.png" alt="" />
+                        <div className="absolute right-0 z-10 -bottom-16 lg:top-24 lg:-left-20">
+                            <img className="w-32 h-32 md:w-40 md:h-40" src="https://cdn.rareblocks.xyz/collection/celebration/images/hero/3/circular-text.png" alt="" />
                         </div>
                     </div>
 
-                    <div class="relative w-full overflow-hidden lg:order-1 h-96 lg:h-auto lg:w-5/12">
-                        <div class="absolute inset-0">
-                            <img class="object-cover w-full h-full scale-150" src="https://cdn.rareblocks.xyz/collection/celebration/images/hero/3/man-working-on-laptop.jpg" alt="" />
+                    <div className="relative w-full overflow-hidden lg:order-1 h-96 lg:h-auto lg:w-5/12">
+                        <div className="absolute inset-0">
+                            <img className="object-cover w-full h-full scale-100" src={landing} alt="" />
                         </div>
 
-                        <div class="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent"></div>
+                        <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent"></div>
 
-                        <div class="absolute bottom-0 left-0">
-                            <div class="p-4 sm:p-6 lg:p-8">
-                                <div class="flex items-center">
-                                    <svg class="w-10 h-10 text-orange-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                        <path fill-rule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z" clip-rule="evenodd" />
+                        <div className="absolute bottom-0 left-0">
+                            <div className="p-4 sm:p-6 lg:p-8">
+                                <div className="flex items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -1 24 24" width={55} height={55} color={"#fab600"} fill={"none"}>
+                                        <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C7.52232 2 3.77426 4.94289 2.5 9H5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                        <path d="M12 8V12L14 14" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                        <path d="M2 12C2 12.3373 2.0152 12.6709 2.04494 13M9 22C8.6584 21.8876 8.32471 21.7564 8 21.6078M3.20939 17C3.01655 16.6284 2.84453 16.2433 2.69497 15.8462M4.83122 19.3065C5.1369 19.6358 5.46306 19.9441 5.80755 20.2292" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                                     </svg>
-                                    <h2 class="font-bold text-white text-7xl ml-2.5">395</h2>
+                                    <h2 className="font-bold text-white text-7xl ml-2.5">24/7</h2>
                                 </div>
-                                <p class="max-w-xs mt-1.5 text-xl text-white">Professionals have organized their desk via PostCra</p>
+                                <p className="max-w-xs mt-1.5 text-xl text-white">Professionals Help Available</p>
                             </div>
                         </div>
                     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: '#080407',
+        background: '#ffcdf3',
+        primary: '#ff7ff7',
+        secondary: '#DE7E5D',
+        accent: '#434343',
+      }
+    },
   },
   plugins: [],
 }
